2bdb6ec7522c9cfd0ad9d64440313979d757df93
최운학 <chldnsgkr7@gmail.com>
2021-03-04 16:07:17 +0900

    최초 커밋

---
+++ src/main/java/com/example/demo/LcpApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcpApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcpApiTestApplication.class, args);
+	}
+
+}
